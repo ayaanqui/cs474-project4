2b43a5b7710c17746c21dbb92e82181114c52fa3
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-12-01 19:28:12 -0600

    read and display pc_input.txt

--- main.cpp
+++ main.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+/**
+ * @brief Reads "pc_input.txt" and returns the contents of this
+ * file a vector of strings, with each line as the element of the array
+ * 
+ * @return std::vector<std::string> 
+ */
+std::vector<std::string> file_contents()
+{
+    std::vector<std::string> data;
+    std::string line;
+    std::ifstream pc_input_file("pc_input.txt");
+    if (!pc_input_file.is_open())
+        return data;
+
+    while (std::getline(pc_input_file, line))
+    {
+        // Ignore lines starting with #
+        if (line[0] == '#')
+            continue;
+        data.push_back(line);
+    }
+    pc_input_file.close();
+    return data;
+}
 
 int main()
 {
-    std::cout << "CS474 Project 4" << std::endl;
+    std::vector<std::string> file_data = file_contents();
+    for (std::string &line : file_data)
+    {
+        std::cout << line << std::endl;
+    }
     return 0;
 }
