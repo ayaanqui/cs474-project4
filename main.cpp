254a79a00fb818fc2664bbbc7394025604ec1304
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-12-02 21:11:29 -0600

    store parsed expressions inside vector

--- main.cpp
+++ main.cpp
@@ -85,11 +85,15 @@ Expression parse_line(const std::string raw_line)
 int main()
 {
     std::vector<std::string> file_data = file_contents();
+    std::vector<Expression> parsed_data;
     for (std::string &line : file_data)
+        parsed_data.push_back(parse_line(line));
+
+    for (Expression &exp : parsed_data)
     {
-        Expression expression = parse_line(line);
-        expression.print();
+        exp.print();
         std::cout << std::endl;
     }
+
     return 0;
 }
