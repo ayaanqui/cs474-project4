068342fcbd3ee11904874ffc68d2c7407b4a6424
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-12-02 23:41:18 -0600

    add basic program loop

--- Program.cpp
+++ Program.cpp
@@ -6,14 +6,37 @@
 
 void Program::startProgram()
 {
+    while (true)
+    {
+        std::string input;
+        std::cout << "> ";
+        std::getline(std::cin, input);
+
+        char input_command = input[0];
+        switch (input_command)
+        {
+        case 'r':
+            std::cout << "Run the entire program" << std::endl;
+            break;
+        case 's':
+            std::cout << "Step by step program execution" << std::endl;
+        case 'x':
+            return;
+        default:
+            std::cout << "Unknown command" << std::endl;
+            break;
+        }
+
+        // Blank line...
+        std::cout << std::endl;
+    }
+}
+
+std::vector<Expression> Program::loadData()
+{
     std::vector<std::string> file_data = this->getFileData("pc_input.txt");
     std::vector<Expression> parsed_data;
     for (std::string &line : file_data)
         parsed_data.push_back(this->parseLine(line));
-
-    for (Expression &exp : parsed_data)
-    {
-        exp.print();
-        std::cout << std::endl;
-    }
+    return parsed_data;
 }
--- Program.h
+++ Program.h
@@ -1,12 +1,15 @@
 #include <vector>
 #include <string>
 #include "Instruction.h"
+#include "expression/Expression.h"
 
 class Program : Instruction
 {
 private:
     int loopCounter, i;
     bool stopLoop;
+
+    std::vector<Expression> loadData();
 
 public:
     void startProgram();
