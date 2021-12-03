ca271af51f42c084c308dbbff82924d95396f248
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-12-02 21:19:59 -0600

    add variable snapshot

---
+++ variable_snapshot/VariableSnaphot.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "VariableSnapshot.h"
+
+VariableSnapshot::VariableSnapshot()
+{
+    w, x, y, z = 0;
+}
+
+VariableSnapshot::VariableSnapshot(int w, int x, int y, int z)
+{
+    this->w = w;
+    this->x = x;
+    this->y = y;
+    this->z = z;
+}
+
+void VariableSnapshot::print()
+{
+    std::cout << "Result:" << std::endl;
+    std::cout << "x = " << this->x << ", "
+              << "y = " << this->y << ", "
+              << "w = " << this->w << " and "
+              << "z = " << this->z
+              << "" << std::endl;
+}
---
+++ variable_snapshot/VariableSnapshot.h
@@ -0,0 +1,12 @@
+class VariableSnapshot
+{
+public:
+    int w;
+    int x;
+    int y;
+    int z;
+
+    VariableSnapshot();
+    VariableSnapshot(int w, int x, int y, int z);
+    void print();
+};
